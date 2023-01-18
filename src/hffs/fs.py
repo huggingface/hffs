528509f59cbb10bcf5648307066a54f2e2a72b64
mariosasko <mariosasko777@gmail.com>
2023-01-18 13:55:53 +0100

    Support repo type in fsspec url

--- src/hffs/fs.py
+++ src/hffs/fs.py
@@ -2,7 +2,7 @@ import collections
 import os
 import tempfile
 from pathlib import PurePosixPath
-from typing import Optional
+from typing import Dict, Optional, Tuple
 from urllib.parse import quote
 
 import fsspec
@@ -11,7 +11,42 @@ import huggingface_hub.constants
 import huggingface_hub.utils
 import requests
 
-from . import __version__
+
+def _repo_type_and_id_from_hf_id(hf_id: str) -> Tuple[Optional[str], Optional[str], str]:
+    """
+    Returns the repo type and ID from a HF ID
+    Args:
+        hf_id (`str`):
+            An URL or ID of a repository on the HF hub. Accepted values are:
+            - <repo_type>/<namespace>/<repo_id>
+            - <namespace>/<repo_id>
+            - <repo_id>
+    Returns:
+        A tuple with two items: repo_type (`str` or `None`), namespace (`str` or
+        `None`) and repo_id (`str`).
+    """
+    url_segments = hf_id.split("/")
+    is_hf_id = len(url_segments) <= 3
+    if is_hf_id:
+        if len(url_segments) == 3:
+            # Passed <repo_type>/<user>/<model_id> or <repo_type>/<org>/<model_id>
+            repo_type, namespace, repo_id = url_segments[-3:]
+        elif len(url_segments) == 2:
+            # Passed <user>/<model_id> or <org>/<model_id>
+            namespace, repo_id = hf_id.split("/")[-2:]
+            repo_type = None
+        else:
+            # Passed <model_id>
+            repo_id = url_segments[0]
+            namespace, repo_type = None, None
+    else:
+        raise ValueError(f"Unable to retrieve user and repo ID from the passed HF ID: {hf_id}")
+
+    if repo_type not in huggingface_hub.constants.REPO_TYPES:
+        assert repo_type is not None, "repo_type `None` do not have mapping"
+        repo_type = huggingface_hub.constants.REPO_TYPES_MAPPING.get(repo_type)
+
+    return repo_type, namespace, repo_id
 
 
 # huggingface_hub.hf_hub_url doesn't support non-default endpoints at the moment
@@ -81,11 +116,11 @@ class HfFileSystem(fsspec.AbstractFileSystem):
     >>> import fsspec
 
     >>> # Read a remote file
-    >>> with fsspec.open("hf://username/my-dataset:/remote/file/in/repo.bin", repo_type="dataset") as f:
+    >>> with fsspec.open("hf://dataset/my-username/my-dataset:/remote/file/in/repo.bin") as f:
     ...     data = f.read()
 
     >>> # Write a remote file
-    >>> with fsspec.open("hf://username/my-dataset:/remote/file/in/repo.bin", "wb", repo_type="dataset") as f:
+    >>> with fsspec.open("hf://dataset/my-username/my-dataset:/remote/file/in/repo.bin", "wb") as f:
     ...     f.write(data)
     ```
     """
@@ -97,10 +132,10 @@ class HfFileSystem(fsspec.AbstractFileSystem):
     def __init__(
         self,
         repo_id: str,
-        endpoint: Optional[str] = None,
         token: Optional[str] = None,
         repo_type: Optional[str] = None,
         revision: Optional[str] = None,
+        endpoint: Optional[str] = None,
         **kwargs,
     ):
         super().__init__(self, **kwargs)
@@ -113,7 +148,7 @@ class HfFileSystem(fsspec.AbstractFileSystem):
         self.token = token if token is not None else huggingface_hub.HfFolder.get_token()
         self.repo_type = repo_type if repo_type is not None else huggingface_hub.constants.REPO_TYPE_MODEL
         self.revision = revision
-        self._api = huggingface_hub.HfApi(endpoint=endpoint)
+        self._api = huggingface_hub.HfApi(endpoint=endpoint, token=token)
 
     def _dircache_from_repo_info(self):
         repo_info = self._api.repo_info(
@@ -153,13 +188,13 @@ class HfFileSystem(fsspec.AbstractFileSystem):
         protocol = cls.protocol
         if path.startswith(protocol + "://"):
             path = path[len(protocol + "://") :]
-            repo_id, *paths = path.split(":/", 1)
+            hf_id, *paths = path.split(":/", 1)
             path = paths[0] if paths else cls.root_marker
         return path
 
     def unstrip_protocol(self, path):
         return super().unstrip_protocol(
-            self.repo_id + ("@" + self.revision if self.revision is not None else "") + ":/" + path
+            f"{self.repo_type}/{self.repo_id}{'@' + self.revision if self.revision is not None else ''}:/{path}"
         )
 
     @staticmethod
@@ -167,10 +202,14 @@ class HfFileSystem(fsspec.AbstractFileSystem):
         protocol = HfFileSystem.protocol
         if path.startswith(protocol + "://"):
             path = path[len(protocol + "://") :]
-        repo_id, *paths = path.split(":/", 1)
-        repo_id, *revisions = repo_id.split("@", 1)
+        hf_id, *paths = path.split(":/", 1)
+        hf_id, *revisions = hf_id.split("@", 1)
         out = {}
+        repo_type, namespace, repo_id = _repo_type_and_id_from_hf_id(hf_id)
+        repo_id = f"{namespace}/{repo_id}" if namespace is not None else repo_id
         out["repo_id"] = repo_id
+        if repo_type is not None:
+            out["repo_type"] = repo_type
         if revisions:
             out["revision"] = revisions[0]
         return out
@@ -182,32 +221,32 @@ class HfFileSystem(fsspec.AbstractFileSystem):
         **kwargs,
     ):
         if mode == "ab":
-            raise NotImplementedError("Appending to remote files is not supported")
+            raise NotImplementedError("Appending to remote files is not yet supported.")
         path = self._strip_protocol(path)
         return HfFile(self, path, mode=mode, **kwargs)
 
-    def _rm(self, path):
+    def _rm(self, path, **kwargs):
         path = self._strip_protocol(path)
         operations = [huggingface_hub.CommitOperationDelete(path_in_repo=path)]
-        commit_message = f"Delete {path} with hffs"
+        commit_message = f"Delete {path}"
         self._api.create_commit(
             repo_id=self.repo_id,
             repo_type=self.repo_type,
             token=self.token,
             operations=operations,
             revision=self.revision,
-            commit_message=commit_message,
+            commit_message=kwargs.pop("commit_message", commit_message),
+            commit_description=kwargs.pop("commit_description", None),
         )
         self.invalidate_cache()
 
-    def rm(self, path, recursive=False, maxdepth=None):
+    def rm(self, path, recursive=False, maxdepth=None, **kwargs):
         paths = self.expand_path(path, recursive=recursive, maxdepth=maxdepth)
         file_paths = [path for path in paths if not self.isdir(path)]
         operations = [huggingface_hub.CommitOperationDelete(path_in_repo=file_path) for file_path in file_paths]
         commit_message = f"Delete {path} "
         commit_message += "recursively " if recursive else ""
         commit_message += f"up to depth {maxdepth} " if maxdepth is not None else ""
-        commit_message += "with hffs"
         # TODO: use `commit_description` to list all the deleted paths?
         self._api.create_commit(
             repo_id=self.repo_id,
@@ -215,7 +254,8 @@ class HfFileSystem(fsspec.AbstractFileSystem):
             token=self.token,
             operations=operations,
             revision=self.revision,
-            commit_message=commit_message,
+            commit_message=kwargs.pop("commit_message", commit_message),
+            commit_description=kwargs.pop("commit_description", None),
         )
         self.invalidate_cache()
 
@@ -234,14 +274,13 @@ class HfFileSystem(fsspec.AbstractFileSystem):
         path1 = self._strip_protocol(path1)
         path2 = self._strip_protocol(path2)
 
+        # TODO: Wait for https://github.com/huggingface/huggingface_hub/issues/1083 to be resolved to simplify this logic
         if self.info(path1)["lfs"] is not None:
-            # Perhaps we can add a CopyOperation to the commit API in huggingace_hub?
-            headers = huggingface_hub.utils.build_hf_headers(
-                use_auth_token=self.token, library_name="hffs", library_version=__version__
-            )
+            headers = huggingface_hub.utils.build_hf_headers(token=self.token, is_write_action=True)
+            commit_message = f"Copy {path1} to {path2}"
             payload = {
-                "summary": f"Copy {path1} to {path2} with hffs",
-                "description": "",
+                "summary": kwargs.pop("commit_message", commit_message),
+                "description": kwargs.pop("commit_description", ""),
                 "files": [],
                 "lfsFiles": [
                     {
@@ -254,7 +293,7 @@ class HfFileSystem(fsspec.AbstractFileSystem):
             }
             revision = self.revision if self.revision is not None else huggingface_hub.constants.DEFAULT_REVISION
             r = requests.post(
-                f"{self.endpoint}/api/{self.repo_type}s/{self.repo_id}/commit/{revision}",
+                f"{self.endpoint}/api/{self.repo_type}s/{self.repo_id}/commit/{quote(revision, safe='')}",
                 json=payload,
                 headers=headers,
             )
@@ -262,6 +301,7 @@ class HfFileSystem(fsspec.AbstractFileSystem):
         else:
             with self.open(path1, "rb") as f:
                 content = f.read()
+            commit_message = f"Copy {path1} to {path2}"
             self._api.upload_file(
                 path_or_fileobj=content,
                 path_in_repo=path2,
@@ -269,7 +309,8 @@ class HfFileSystem(fsspec.AbstractFileSystem):
                 token=self.token,
                 repo_type=self.repo_type,
                 revision=self.revision,
-                commit_message=f"Copy {path1} to {path2} with hffs",
+                commit_message=kwargs.pop("commit_message", commit_message),
+                commit_description=kwargs.pop("commit_description", None),
             )
         self.invalidate_cache()
 
@@ -278,9 +319,7 @@ class HfFile(fsspec.spec.AbstractBufferedFile):
     def _fetch_range(self, start, end):
         headers = {
             "range": f"bytes={start}-{end}",
-            **huggingface_hub.utils.build_hf_headers(
-                use_auth_token=self.fs.token, library_name="hffs", library_version=__version__
-            ),
+            **huggingface_hub.utils.build_hf_headers(token=self.fs.token),
         }
         url = _path_to_http_url(
             self.path,
@@ -294,25 +333,15 @@ class HfFile(fsspec.spec.AbstractBufferedFile):
         return r.content
 
     def _initiate_upload(self):
-        self.temp_file = tempfile.NamedTemporaryFile(delete=False)
+        self.temp_file = tempfile.NamedTemporaryFile(dir=huggingface_hub.constants.HUGGINGFACE_HUB_CACHE, delete=False)
 
     def _upload_chunk(self, final=False):
         self.buffer.seek(0)
         block = self.buffer.read()
-        # if final and len(block) == self.loc:
-        #     # delegate first flush to `upload_file` if possible
-        #     self.fs._api.upload_file(
-        #         path_or_fileobj=self.temp_file.name,
-        #         path_in_repo=self.path,
-        #         repo_id=self.fs.repo_id,
-        #         token=self.fs.token,
-        #         repo_type=self.fs.repo_type,
-        #         revision=self.fs.revision,
-        #         commit_message=f"Upload {self.path} with hffs",
-        #     )
         self.temp_file.write(block)
         if final:
             self.temp_file.close()
+            commit_message = f"Upload {self.path}"
             self.fs._api.upload_file(
                 path_or_fileobj=self.temp_file.name,
                 path_in_repo=self.path,
@@ -320,7 +349,13 @@ class HfFile(fsspec.spec.AbstractBufferedFile):
                 token=self.fs.token,
                 repo_type=self.fs.repo_type,
                 revision=self.fs.revision,
-                commit_message=f"Upload {self.path} with hffs",
+                commit_message=self.kwargs.pop("commit_message", commit_message),
+                commit_description=self.kwargs.pop("commit_description", None),
             )
             os.remove(self.temp_file.name)
             self.fs.invalidate_cache()
+
+
+# parquet issue by severo
+# token
+# docs
